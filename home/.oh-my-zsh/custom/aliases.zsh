c38f81e1542bae5ac23eda768100b4725a4ce496
Eric Matthys <ericmatthys@gmail.com>
2014-12-29 14:59:03 -0700

    Added zsh configuration.

--- home/.oh-my-zsh/custom/aliases.zsh
+++ home/.oh-my-zsh/custom/aliases.zsh
@@ -1,6 +1,6 @@
-alias reload='source ~/.zshrc'
+alias reload="source ~/.zshrc"
 
 # Projects
-alias plex-web-client='cd ~/Dev/plex/plex-web-client'
-alias plex-media-server='cd ~/Dev/plex/plex-media-server'
-alias my-plex='cd ~/Dev/plex/my-plex'
+alias plex-web-client="cd ~/Dev/plex/plex-web-client"
+alias plex-media-server="cd ~/Dev/plex/plex-media-server"
+alias my-plex="cd ~/Dev/plex/my-plex"
