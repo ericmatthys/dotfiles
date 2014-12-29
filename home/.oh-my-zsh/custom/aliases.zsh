eeafdbd4b30ab5fa1de971ca0b97de5e1a6b9c23
Eric Matthys <ericmatthys@gmail.com>
2014-12-29 12:34:23 -0700

    Added custom aliases.

---
+++ home/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,6 @@
+alias reload='source ~/.zshrc'
+
+# Projects
+alias plex-web-client='cd ~/Dev/plex/plex-web-client'
+alias plex-media-server='cd ~/Dev/plex/plex-media-server'
+alias my-plex='cd ~/Dev/plex/my-plex'
